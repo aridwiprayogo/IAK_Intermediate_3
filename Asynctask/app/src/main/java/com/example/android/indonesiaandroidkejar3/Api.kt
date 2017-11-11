6db4aeac52440d0d65eb6e57fb0f4c4435f792f1
aridwiprayogo <kisamberbledek@yahoo.com>
2017-11-11 14:45:12 +0700

    pertemuan 2 RecyclerView dan Intent

--- Asynctask/app/src/main/java/com/example/android/indonesiaandroidkejar3/Api.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android.indonesiaandroidkejar3
-
-/**
- * Created by MYWINDOWS on 05/11/2017.
- */
-
-internal object Api {
-    var url = "https://api.bukalapak.com/v2/products.json?page=1&per_page=10keywords=electronic"
-}
--- Asynctask/app/src/test/java/com/example/android/indonesiaandroidkejar3/MainActivityTest.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.indonesiaandroidkejar3;
-
-/**
- * Created by MYWINDOWS on 05/11/2017.
- */
-public class MainActivityTest {
-
-}
